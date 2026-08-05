0fe96fbb139aa472672c0e6ebd69696500c3b703
Callum Prentice <callum@lindenlab.com>
2026-08-05 15:42:26 -0700

    Update the batch file that builds everything to (a) use a more modern CMake paradigm and (b) match the instructions in the README.md

--- tools/build.bat
+++ tools/build.bat
@@ -3,17 +3,15 @@
     @goto end
 )
 
+@rem Note: let's not make the build directory a variable with this line in play - just in case...
 if exist build rmdir build /s /q
-mkdir build
-pushd build
 
-cmake^
- -G "Visual Studio 17 2022" -A x64 ..^
- -DCEF_RUNTIME_LIBRARY_FLAG=/MD^
- -DUSE_SANDBOX=Off^
- -DCEF_PACKAGE_URL=https://cef-builds.spotifycdn.com/cef_binary_150.0.9%%2Bg81b0088%%2Bchromium-150.0.7871.46_windows64_minimal.tar.bz2
+@rem Note: CEF packages canbe found at the Spotify Automated Builds site here: https://cef-builds.spotifycdn.com/index.html
+@rem A batch file restriction means you must replace the % signs in the original url with %%
+set CEF_MINIMAL_URL=https://cef-builds.spotifycdn.com/cef_binary_150.0.9%%2Bg81b0088%%2Bchromium-150.0.7871.46_windows64_minimal.tar.bz2
 
-cmake --build . --config Release
+cmake -B build -G "Visual Studio 17 2022" -A x64  -DCEF_RUNTIME_LIBRARY_FLAG=/MD -DUSE_SANDBOX=OFF -DCEF_PACKAGE_URL=%CEF_MINIMAL_URL%
+cmake --build build --config Release
 
 :end
 popd
