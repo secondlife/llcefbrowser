134501a0548393d407f4719e191de1f47d995a0d
Callum Prentice <callum@lindenlab.com>
2026-08-10 15:09:46 -0700

    Update the CEF we build against to 150.0.17 (from 150.0.9)

--- tools/build.bat
+++ tools/build.bat
@@ -8,7 +8,7 @@ if exist build rmdir build /s /q
 
 @rem Note: CEF packages canbe found at the Spotify Automated Builds site here: https://cef-builds.spotifycdn.com/index.html
 @rem A batch file restriction means you must replace the % signs in the original url with %%
-set CEF_MINIMAL_URL=https://cef-builds.spotifycdn.com/cef_binary_150.0.9%%2Bg81b0088%%2Bchromium-150.0.7871.46_windows64_minimal.tar.bz2
+set CEF_MINIMAL_URL=https://cef-builds.spotifycdn.com/cef_binary_150.0.17%%2Bg94c1726%%2Bchromium-150.0.7871.187_windows64_minimal.tar.bz2
 
 cmake -B build -G "Visual Studio 17 2022" -A x64  -DCEF_RUNTIME_LIBRARY_FLAG=/MD -DUSE_SANDBOX=OFF -DCEF_PACKAGE_URL=%CEF_MINIMAL_URL%
 cmake --build build --config Release
